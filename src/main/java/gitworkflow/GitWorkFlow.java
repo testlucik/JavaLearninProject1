63f92bfbc3c424f691bb7c74d630e8bfcb3e65be
testlucik <testluciktest@gmail.com>
2023-09-21 20:57:20 +0300

    Defined the git workflow for future development strategy (2)

--- src/main/java/gitworkflow/GitWorkFlow.java
+++ src/main/java/gitworkflow/GitWorkFlow.java
@@ -2,21 +2,23 @@ package gitworkflow;
 
 public class GitWorkFlow {
     public static void main(String[] args) {
-        String step1 = "Perform git stash - to store uncommitted changes";
-        String step2 = "Perform git checkout master - to move to the local master branch";
-        String step3 = "Perform git fetch command - to fetch the origin branches";
-        String step4 = "Perform git checkout -b new_branch_name command - to create and instantly move to a new branch, " + "that is identical to local master branch";
-        String step5 = "Add your changes that relates to the task";
-        String step6 = "Review your changes and check your code before committing them";
-        String step7 = "Execute git commit by using the Intellij interface, where you can select what changes you want to commit";
-        String step8 = "Once all the changes are committed, execute a git push command using CTRL+AY+K hot keys";
-        String step9 = "The committed changes are pushed to an origin branch with the same name, navigate to the Github branch";
-        String step10 = "Create a Pull Request from origin/new_branch_name into origin/master";
-        String step11 = "Add the Pull Request details - name, description and request review from our colleagues";
-        String step12 = "If you collect at least 1 approval from your colleagues then merge the branch";
-        String step13 = "After that, the origin/master contains the latest changes and the origin/new_branch and the origin/new_branch_name can be deleted";
-        String step14 = "Do not forget that locally you are still on the new_branch_name, it's good to move back to master";
-        String step15 = "However, the local master branch does not have the latest changes,therefore perform a git fetch and git pull";
+        String step1 =  "Execute git stash in the console";
+        String step2 = "Perform checkout master - to move to the local master branch";
+        String step3 = "Execute git fetch command - to see if there are any changes on remote/origin";
+        String step4 = "Execute git pull - this will download the latest changes from origin/master on local master";
+        String step5 = "Execute git branch New_Branch_Name - as a result we redirected to the new branch";
+        String step6 = "Perform the checkout New_Branch_Name changes in the solution";
+        String step7 = "Add the task related changes in the solution";
+        String step8 = "Execute git commit command rom the Intellij Idea to store the chnages at a local level - on the current branch";
+        String step9 = "Execute git push from Intellij Idea panel - as a result, anew remote branch is created with origin/New_Branch_Name";
+        String step10 = "Create a pull requiest from origini/New_Branch_Name into master";
+        String step11 = "Add the pull Request details like name, description and reviewers";
+        String step12 = "If we collect at least 1 approval then we can merge the changes and continue wit another task";
+        String step13 ="Do not forget the locally you are still on the New_Branch_Name";
+        String step14 = "If someone asks for changes then we should open the project and make sure that are on the same New_Branch_Name branch";
+        String step15 = "Change the requested lines of code";
+        String step16 = "Perform a git commit amend to change the latest changes";
+        String step17 = "Perform git push to update the status";
 
 
         System.out.println(step1);
@@ -34,5 +36,7 @@ public class GitWorkFlow {
         System.out.println(step13);
         System.out.println(step14);
         System.out.println(step15);
+        System.out.println(step16);
+        System.out.println(step17);
     }
 }
